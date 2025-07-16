bcd0acbe770801cb5760eac2e4a0232b823acbcd
Saurav207 <saurav2071996@gmail.com>
2025-07-16 11:01:42 +0530

    Functional Component & React Element

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-const heading = React.createElement(
-    "h1",
-    {id: "parent"}, [
-        React.createElement("h1", {id: "first-child"},[
-        React.createElement("h1", {id: "child1"}, "I'm in h1 tag"),
-        React.createElement("h2", {id: "child2"}, "I'm in h2 tag"),
-    ]),
-    React.createElement("h1", {id: "second-child"},[
-        React.createElement("h1", {id: "child1"}, "I'm in h1 tag"),
-        React.createElement("h2", {id: "child2"}, "I'm in h2 tag"),
-    ])]
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    This is using JSX..
+  </h1>
+);
+const number = 11;
+
+//React Functional Component
+const HeadingCom = () => (
+  <div className="container">
+    {number}
+    <h1 className="heading">Hello this is Functional Component</h1>
+  </div>
 );
 
-console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(heading);
+root.render(<HeadingCom />);
